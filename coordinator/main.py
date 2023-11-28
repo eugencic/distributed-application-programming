013460db2a95aeb06d9290b3aaa3441859ffe61b
Eugen <72609351+eugencic@users.noreply.github.com>
2023-11-28 18:06:00 +0200

    fix coordinator to work correctly

--- coordinator/main.py
+++ coordinator/main.py
@@ -1,7 +1,7 @@
 import grpc
-from concurrent import futures
-from flask import Flask, request, jsonify, Response
-import threading
+from concurrent.futures import ThreadPoolExecutor, wait
+from flask import Flask, request, jsonify
+
 import traffic_analytics_pb2
 import traffic_analytics_pb2_grpc
 import traffic_regulation_pb2
@@ -9,60 +9,30 @@ import traffic_regulation_pb2_grpc
 
 app = Flask(__name__)
 
-# TRAFFIC_ANALYTICS_SERVICE_ADDRESS = "traffic-analytics-service:7000"
-# TRAFFIC_REGULATION_SERVICE_ADDRESS = "traffic-regulation-service:8000"
-
 TRAFFIC_ANALYTICS_SERVICE_ADDRESS = "localhost:7071"
 TRAFFIC_REGULATION_SERVICE_ADDRESS = "localhost:8081"
 
 
-def async_health_check_grpc(service_address):
-    def _make_request():
-        channel = grpc.insecure_channel(service_address)
-        try:
-            grpc.channel_ready_future(channel).result(timeout=1)
-            print(f"Health check for {service_address} successful.")
-        except Exception as e:
-            print(f"Health check for {service_address} failed: {str(e)}")
-
-    thread = threading.Thread(target=_make_request)
-    thread.start()
-
-
 def add_traffic_data_analytics(intersection_id, message):
-    try:
-        channel = grpc.insecure_channel(TRAFFIC_ANALYTICS_SERVICE_ADDRESS)
-        stub = traffic_analytics_pb2_grpc.TrafficAnalyticsStub(channel)
-        request = traffic_analytics_pb2.AddDataAnalyticsRequest(
-            intersection_id=intersection_id,
-            message=message,
-        )
-        response = stub.AddDataAnalytics(request)
-        return response
-    except grpc.RpcError as e:
-        print("Error in add_traffic_data_analytics:")
-        import traceback
-        traceback.print_exc()
-        return e
-
+    channel = grpc.insecure_channel(TRAFFIC_ANALYTICS_SERVICE_ADDRESS)
+    stub = traffic_analytics_pb2_grpc.TrafficAnalyticsStub(channel)
+    request = traffic_analytics_pb2.AddDataAnalyticsRequest(
+        intersection_id=intersection_id,
+        message=message,
+    )
+    response = stub.AddDataAnalytics(request)
+    return response
 
 
 def add_traffic_data_regulation(intersection_id, message):
-    try:
-        channel = grpc.insecure_channel(TRAFFIC_REGULATION_SERVICE_ADDRESS)
-        stub = traffic_regulation_pb2_grpc.TrafficRegulationStub(channel)
-        request = traffic_regulation_pb2.AddDataRegulationRequest(
-            intersection_id=intersection_id,
-            message=message,
-        )
-        response = stub.AddDataRegulation(request)
-        return response
-    except grpc.RpcError as e:
-        print("Error in add_traffic_data_regulation:")
-        import traceback
-        traceback.print_exc()
-        return e
-
+    channel = grpc.insecure_channel(TRAFFIC_REGULATION_SERVICE_ADDRESS)
+    stub = traffic_regulation_pb2_grpc.TrafficRegulationStub(channel)
+    request = traffic_regulation_pb2.AddDataRegulationRequest(
+        intersection_id=intersection_id,
+        message=message,
+    )
+    response = stub.AddDataRegulation(request)
+    return response
 
 
 def undo_traffic_data_analytics(intersection_id):
@@ -72,10 +42,7 @@ def undo_traffic_data_analytics(intersection_id):
         intersection_id=intersection_id,
     )
     response = stub.DeleteDataAnalytics(request)
-    print("Undo analytics:")
-    print(response)
     return response
-
 
 
 def undo_traffic_data_regulation(intersection_id):
@@ -85,49 +52,62 @@ def undo_traffic_data_regulation(intersection_id):
         intersection_id=intersection_id,
     )
     response = stub.DeleteDataRegulation(request)
-    print("Undo regulation:")
-    print(response)
     return response
 
 
-@app.route('/coordinator/add_data', methods=['POST'])
+def saga_coordinator(intersection_id, message):
+    try:
+        print("Step 1: Add data to Traffic Analytics")
+        with ThreadPoolExecutor() as executor:
+            future_analytics = executor.submit(add_traffic_data_analytics, intersection_id, message)
+
+        print("Step 2: Add data to Traffic Regulation")
+        with ThreadPoolExecutor() as executor:
+            future_regulations = executor.submit(add_traffic_data_regulation, intersection_id, message)
+
+        wait([future_analytics, future_regulations])
+
+        print("Steps Completed. Proceeding to compensating actions if needed.")
+
+        future_analytics.result()
+        future_regulations.result()
+
+        print("No nee for compensating actions.")
+
+        return 200
+
+    except Exception as e:
+        print("Exception during saga coordination:", str(e))
+        print("Compensate: Undo changes made by previous steps...")
+
+        with ThreadPoolExecutor() as compensator_executor:
+            future_analytics = compensator_executor.submit(undo_traffic_data_analytics, intersection_id)
+            future_regulations = compensator_executor.submit(undo_traffic_data_regulation, intersection_id)
+
+            wait([future_analytics, future_regulations])
+
+            print("Compensating actions completed.")
+
+            return 500
+
+
+@app.route('/add_data', methods=['POST'])
 def add_data():
     try:
         data = request.json
         intersection_id = data.get('intersection_id')
         message = data.get('message')
 
-        # Step 1: Asynchronous health checks
-        thread_analytics = threading.Thread(target=lambda: async_health_check_grpc(TRAFFIC_ANALYTICS_SERVICE_ADDRESS))
-        thread_regulation = threading.Thread(target=lambda: async_health_check_grpc(TRAFFIC_REGULATION_SERVICE_ADDRESS))
-        thread_analytics.start()
-        thread_regulation.start()
-        # Step 2: Synchronous requests to save data
-        response_analytics = add_traffic_data_analytics(intersection_id, message)
-        response_regulations = add_traffic_data_regulation(intersection_id, message)
-        print("Response analytics:")
-        print(response_analytics)
-        print("Response regulations:")
-        print(response_regulations)
-        thread_analytics.join()
-        thread_regulation.join()
-        # Check if any health check failed
-        if isinstance(response_analytics, grpc.RpcError) or isinstance(response_regulations, grpc.RpcError):
-            # Undo changes by sending undo requests to both services
-            undo_analytics = undo_traffic_data_analytics(intersection_id)
-            undo_regulation = undo_traffic_data_regulation(intersection_id)
+        print("Initiate Saga")
+        response = saga_coordinator(intersection_id, message)
 
-            # Print responses for both services
-            print("Undo analytics:")
-            print(undo_analytics)
-            print("Undo regulations:")
-            print(undo_regulation)
-            return jsonify({"status": "error", "message": "Connection health check failed. Changes undone."}), 500
+        if response == 500:
+            return jsonify({"message": "Internal Server Error"}), 500
 
-        return jsonify({"status": "success", "message": "Data added successfully."})
+        return jsonify({"message": "Data added successfully"}), 200
 
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
+        return jsonify({"message": "Internal Server Error"}), 500
 
 
 if __name__ == '__main__':
